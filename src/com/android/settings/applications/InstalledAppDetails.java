b2be995e41f723d8bd880d30f192a1150d67bb67
Steve Kondik <shade@chemlab.org>
2013-06-26 11:31:01 -0700

    privacy guard: Fix crash when uninstalling an app
     * Don't try to update the privacy guard flag while refreshing after an
       uninstall since the PM will throw an exception at us.
    
    Change-Id: Idadb99fefc2861e871f6206b08c844dda742fa68

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -871,6 +871,12 @@ public class InstalledAppDetails extends Fragment
             }
         }
 
+
+        // only setup the privacy guard setting if we didn't get uninstalled
+        if (!mMoveInProgress) {
+            initPrivacyGuardButton();
+        }
+
         return true;
     }
 
@@ -1011,7 +1017,6 @@ public class InstalledAppDetails extends Fragment
             initDataButtons();
             initMoveButton();
             initNotificationButton();
-            initPrivacyGuardButton();
         } else {
             mMoveAppButton.setText(R.string.moving);
             mMoveAppButton.setEnabled(false);
